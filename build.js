3dcf86cfaeb6d5acf0c4cf147bc8a17832ee5343
Antono Vasiljev <antono.vasiljev@gmail.com>
2015-04-21 00:02:44 +0300

    Use promises for sequential actions

--- build.js
+++ build.js
@@ -3,45 +3,65 @@ var http   = require('https');
 var fs     = require('fs');
 var xml2js = require('xml2js');
 
-
-var font = "https://raw.githubusercontent.com/driftyco/ionicons/master/fonts/ionicons.svg";
-
-function getSvgFont() {
-  http.get(font, function(res) {
-    var src = fs.createWriteStream('./src.svg');
-    res.on('data', src.write.bind(src));
-
-    console.log("Got response: " + res.statusCode);
-  }).on('error', function(e) {
-    console.log("GET: " + font);
-    console.log("Cannot download ionicon webfont: " + e.message);
+function getSvgFont(font) {
+  var promise = new Promise(function(resolve, reject) {
+    http.get(font, function(res) {
+      var result = "";
+      res.on('data', function (data) {
+        result += data.toString();
+      });
+      res.on('end', function () {
+        resolve(result);
+      });
+    }).on('error', function(e) {
+      reject(e);
+    });
   });
+  return promise;
 }
 
-function compileTemplate(locals) {
-  var tpl = jade.compileFile(__dirname + '/tpl/element.jade', { pretty: true });
-
-  var res = tpl({
-    iconsetId: 'core-iconset-ionicons',
-    defaultIconSize: '25',
-    glyphs: locals
-  });
-  console.log(res);
-  return res;
-}
-
-function withGlyphs(cb) {
+function parseFont(data) {
   var parser = new xml2js.Parser();
   var glyphs = [];
 
-  fs.readFile(__dirname + '/src.svg', function(err, data) {
+  var promise = new Promise(function (resolve, reject) {
     parser.parseString(data, function (err, result) {
-      result.svg.defs[0].font[0].glyph.forEach(function(g) {
-        glyphs.push(g['$']);
-      })
+      if (err) {
+        reject(err);
+      } else {
+        result.svg.defs[0].font[0].glyph.forEach(function(g) {
+          glyphs.push(g['$']);
+        });
+        resolve(glyphs);
+      }
     });
-    cb(glyphs)
   });
+  return promise;
 }
 
-console.log(withGlyphs(compileTemplate));
+function renderElement(locals) {
+  var tpl = jade.compileFile(__dirname + '/tpl/element.jade', { pretty: true });
+
+  var promise = new Promise(function (resolve, reject) {
+    try {
+      var res = tpl({
+        iconsetId: 'core-iconset-ionicons',
+        defaultIconSize: '25',
+        glyphs: locals
+      });
+      resolve(res);
+    } catch (err) {
+      reject(err);
+    }
+  });
+  return promise;
+}
+
+
+var font = "https://raw.githubusercontent.com/driftyco/ionicons/master/fonts/ionicons.svg";
+var log  = console.log.bind(console);
+
+getSvgFont(font)
+  .then(parseFont, log)
+  .then(renderElement, log)
+  .then(log, log);
